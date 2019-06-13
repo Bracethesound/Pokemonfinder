7e247072e08830a1423158c433a8be56143649df
Jonathan Monroy <jonathanmonroy@Jonathans-MacBook-Air.local>
2019-06-13 13:53:10 -0700

    styled the page

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Why hello there"/>
-    <card> </card>
+    <div class="container">
+      <h1 class="text-center display-1 m-y"> Rock Type Pokemon </h1>
+      <div class="row">
+        <card :url="item.pokemon.url" v-for="item in pokemonRockType" :key="item"> </card>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import card from './components/card.vue'
-export default {
-  name: 'app',
-  components: {
-    HelloWorld,
-    card
+  import HelloWorld from './components/HelloWorld.vue'
+  import card from './components/card.vue'
+  export default {
+    name: 'app',
+    components: {
+      HelloWorld,
+      card
+    },
+    data: function(){
+      return{
+        pokemonRockType: ""
+      }
+    },
+    mounted: function () {
+      console.log("mounted function ran")
+    
+      const axios = require('axios');
+      const vm = this;
+    // Code that will run only after the
+    // entire view has been rendered
+    axios({
+      method: 'get',
+      url: 'https://pokeapi.co/api/v2/type/rock',
+      responseType: 'stream'
+    })
+    .then(function (response) {
+      console.log (response.data);
+      vm.pokemonRockType = response.data.pokemon
+    });
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
